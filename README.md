acf16cff0eec0c3397232819e2fbabd948fed44b
UFAX24 <87823194+UFAX24@users.noreply.github.com>
2021-08-11 20:20:18 +0700

    o

--- README.md
+++
@@ -1,2 +0,0 @@
-# ufacasino
-SKT PNCK
--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con= mysqli_connect("152.228.222.55","gclubson_gclubsonline","fo6b-i*A^pnu","gclubson_spin-test") or die("Error: " . mysqli_error($con));
-mysqli_query($con, "SET NAMES 'utf8' ");
-error_reporting( error_reporting() & ~E_NOTICE );
-date_default_timezone_set('Asia/Bangkok');
-?>
